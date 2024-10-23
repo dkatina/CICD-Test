b87faf1b94322e0a01871ac624e11204ee9cc24c
dkatina <108095842+dkatina@users.noreply.github.com>
2024-10-23 19:59:27 -0400

    added CRUD for Member

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
 from typing import List
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import ValidationError
 from sqlalchemy import Column, select
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 import os
 from datetime import date
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 class Base(DeclarativeBase):
     pass
 
 
 db = SQLAlchemy(model_class = Base)
+ma = Marshmallow()
 
 db.init_app(app) #Adding sqlalchemy extension to Flask
-
+ma.init_app(app)
 
 #========== Models ==========
 
@@ -64,68 +67,98 @@ class Book(Base):
 
     loans: Mapped[List['Loan']] = db.relationship(secondary=loan_book, back_populates='books')
 
+#========== Schemas ==========
+
+#Validation, ensuring that incoming data satisfies what is required to create an instance for the desired
+#Deserialization (converting incoming JSON into python)
+#Serialization (Converting Model Objects into JSON)
+
+class MemberSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Member
+
+class LoanSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Loan
+
+class BookSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Book
+
+member_schema = MemberSchema()
+members_schema = MemberSchema(many=True)
+loan_schema = LoanSchema()
+loans_schema = LoanSchema(many=True)
+book_schema = BookSchema()
+books_schema = BookSchema(many=True)
 
 
-with app.app_context():
-    # new_user = Member(name='Paul', email='paul@email', phone='123')
-    # db.session.add(new_user)
-    # db.session.commit()
+#========= ROUTES ===========
 
-    #READING DATA
-    # query = select(Member).where(Member.name == 'Dylan')
-    # dylan = db.session.execute(query).scalars().first()
-
-    # print(dylan.email)
-
-
-    #UPDATE DATA
-    # dylan2 = db.session.get(Member, 1) #you can only do this for Primary keys
-
-    # print(dylan2.name)
-    # dylan2.name = 'DYLAN KATINA'
-    # db.session.commit()
-    # print(dylan2.name)
-
-    #DELETE DATA
-    # query = select(Member).where(Member.email == 'jilldoe@email.com')
-    # jill = db.session.execute(query).scalars().first()
-
-    # print(jill.name)
-    # db.session.delete(jill)
-    # db.session.commit()
-
-    # new_loan = Loan(loan_date=date.today(), due_date=date.today(), member_id=1)
-    # db.session.add(new_loan)
-    # db.session.commit()
-
-    #ADDING TO MANY-TO-MANY RELATIONSHIPS
-    # book1 = db.session.get(Book, 3)
-    # book2 = db.session.get(Book, 2)
-    # book3 = db.session.get(Book, 1)
-
-    # loan = db.session.get(Loan, 1)
-    # loan.books.append(book1)
-    # loan.books.append(book2)
-    # loan.books.append(book3)
+#CREATE Member
+@app.route("/members", methods=['POST'])
+def create_member():
+    #Validate and Deserialize incoming data
+    try:
+        member_data = member_schema.load(request.json)
+    #If data invalid respond with error message
+    except ValidationError as e:
+        return jsonify(e.messages), 400
     
-    # db.session.commit()
+    #If data is valid, create new member with that data
+    new_member = Member(name=member_data['name'], email=member_data['email'], phone=member_data['phone'])
+    db.session.add(new_member) #Add to session
+    db.session.commit() #commit session to db
 
-    # dylan = db.session.get(Member,1)
+    return member_schema.jsonify(new_member), 201 #return new_member object as a response
 
-    # for loan in dylan.loans:
-    #     print("Loan ID:", loan.id)
-    #     for book in loan.books:
-    #         print(book.title)
-    # user = db.session.get(Member, 1)
-    # print(user.name)
-    pass
+#RETRIEVE USERS
+@app.route("/members", methods=["GET"])
+def get_members():
+    query = select(Member)
+    members = db.session.execute(query).scalars().all()
 
+    return members_schema.jsonify(members), 200
+
+#RETIEVE SPECIFIC USER localhost/members/1
+@app.route("/members/<int:member_id>", methods=['GET'])
+def get_member(member_id):
+    member = db.session.get(Member, member_id)
+
+    return member_schema.jsonify(member), 200
+
+#UPDATE MEMBER
+@app.route("/members/<int:member_id>", methods=['PUT'])
+def update_member(member_id):
+    member = db.session.get(Member, member_id)
+
+    if member == None:
+        return jsonify({"message": "invalid id"}), 400
     
-
-
-
+    try:
+        member_data = member_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
     
-   
+    for field, value in member_data.items():
+        if value:
+            setattr(member, field, value)
+
+    db.session.commit()
+    return member_schema.jsonify(member), 200
+
+#DELETE MEMBER
+@app.route("/members/<int:member_id>", methods=['DELETE'])
+def delete_member(member_id):
+    member = db.session.get(Member, member_id)
+
+    if member == None:
+        return jsonify({"message": "invalid id"}), 400
+
+    db.session.delete(member)
+    db.session.commit()
+    return jsonify({"message": f"succeffuly deleted user {member_id}!"})
+
 
 
 if __name__ == '__main__':
@@ -135,7 +168,7 @@ if __name__ == '__main__':
         
         
 
-    app.run()
+    app.run(debug=True)
 
 
     
